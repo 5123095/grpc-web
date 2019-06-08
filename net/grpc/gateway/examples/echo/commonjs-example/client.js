b87149c95bb50cc56fbfc1963f2833598b573705
xiongchuan <5123095@qq.com>
2019-06-08 12:16:17 +0800

    update proxy and client

--- net/grpc/gateway/examples/echo/commonjs-example/client.js
+++ net/grpc/gateway/examples/echo/commonjs-example/client.js
@@ -23,7 +23,7 @@ const {EchoApp} = require('../echoapp.js');
 const grpc = {};
 grpc.web = require('grpc-web');
 
-var echoService = new EchoServiceClient('http://'+window.location.hostname+':8080', null, null);
+var echoService = new EchoServiceClient('http://dkjian.com:9001', null, null);
 
 var echoApp = new EchoApp(
   echoService,
